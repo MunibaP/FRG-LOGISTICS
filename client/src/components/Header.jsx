aa336df8c6010401394cf1fbcb881b3c31772899
MunibaP <munibapervez596@gmail.com>
2025-07-31 22:02:50 -0400

    changed the logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { NavLink, useLocation, useNavigate } from "react-router-dom"; 
-import logo from "../assets/logo1.png";
+import logo from "../assets/logoChange4.png";
 import "../styles/Header.css"
 
 // Header component for navigation bar
